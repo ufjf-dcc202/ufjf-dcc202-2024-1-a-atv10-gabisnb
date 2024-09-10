4be96e2e265ae2113c450cd70d94347a87f5fa57
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2024-09-10 16:12:44 -0300

    Adiciona main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { get } from "node:http";
+import { deJoaoParaMaria, getJoao, getMaria } from "./joaoEMaria";
+
+console.log('módulo main');
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maria tem ', getMaria(), ' maçãs!');
+
+deJoaoParaMaria();
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maria tem ', getMaria(), ' maçãs!');
